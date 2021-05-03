b86dc8db7998d1471a6563f6de98fdadb2f456c1
Gustavo <gustavo@coursera-something.com>
2021-05-03 20:34:39 -0300

    Erro no app.component.html

--- ecommerce/src/app/componentes/best-seller/best-seller.component.ts
+++ ecommerce/src/app/componentes/best-seller/best-seller.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-best-seller',
+  selector: 'eco-best-seller',
   templateUrl: './best-seller.component.html',
   styleUrls: ['./best-seller.component.css']
 })
